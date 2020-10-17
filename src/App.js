c53ae540ba56ad19a824a5437cf2353ce2ca105e
lukchanpirom <luk.cpr@gmail.com>
2020-10-17 16:17:42 +0700

    V1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "97d75a6588c16cef472567b546d507b2";
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataAPI: [],
+    };
+
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  showMovies(url) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          dataAPI: data.results,
+        });
+      });
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    
+    const searchValue = document.getElementById('seacrh').value;
+    this.showMovies(SEARCH_API + searchValue);
+  }
+
+  componentDidMount() {
+    this.showMovies(API_URL);
+  }
+
+  render() {
+    const { dataAPI } = this.state;
+    const IMAGE_PATH = "https://image.tmdb.org/t/p/w500/";
+
+    return (
+      <div className="wrapper">
+        <header>
+          <h2>Movie</h2>
+          <form onSubmit={this.submitForm}>
+            <input id="seacrh" type="text" placeholder="Search" />
+          </form>
+        </header>
+        <main>
+          {dataAPI.map((element) => {
+            return (
+              <section>
+                <img src={IMAGE_PATH + element.poster_path} />
+                <h2>{element.title}</h2>
+              </section>
+            );
+          })}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
